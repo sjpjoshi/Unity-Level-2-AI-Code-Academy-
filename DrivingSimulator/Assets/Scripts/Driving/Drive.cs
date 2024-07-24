e6ac58ea1358ff874eddb2eba10dd9d445e997e1
suraj.joshi <suraj.p.joshi.27@gmail.com>
2024-07-24 10:44:07 -0700

    Commit -Lesson 3

--- DrivingSimulator/Assets/Scripts/Driving/Drive.cs
+++ DrivingSimulator/Assets/Scripts/Driving/Drive.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ public class Drive : MonoBehaviour {
 
     // Car Variables
     public float speed = 1;
+    private float acceleration;
     private Rigidbody rb;
    
     void Awake() {
@@ -14,24 +16,32 @@ public class Drive : MonoBehaviour {
       
     } // Start
 
+    private void Start() {
+        acceleration = 0.02f;
+
+    } // Start
+
     // Update is called once per frame
     void Update() {
-        if (Input.GetKey(KeyCode.UpArrow)) {
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+            speed = speed + (acceleration * Time.deltaTime);
             rb.AddForce(transform.forward * speed * rb.mass, ForceMode.Impulse); // move foward with velocity
 
         } // if
 
-        if (Input.GetKey(KeyCode.DownArrow)) {
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) ) {
+            speed = speed - (acceleration * Time.deltaTime);
             rb.AddForce(transform.forward * speed * -rb.mass, ForceMode.Impulse); // move foward with velocity
 
         } // if
 
-        if (Input.GetKey(KeyCode.RightArrow)) {
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) ) {
             rb.transform.Rotate(0.0f, 0.4f, 0.0f);
 
         } // if
 
-        if (Input.GetKey(KeyCode.LeftArrow)) {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) ) {
             rb.transform.Rotate(0.0f, -0.4f, 0,0f);
 
         }// if
